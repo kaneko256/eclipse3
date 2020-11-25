45b3bb28f16778ff421193824923a5e46835e7e0
kaneko256 <57160296+kaneko256@users.noreply.github.com>
2020-11-26 08:48:26 +0900

    Add a comment

--- src/main/java/com/github/eclipse3/App.java
+++ src/main/java/com/github/eclipse3/App.java
@@ -4,10 +4,9 @@ package com.github.eclipse3;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		// Added a comment
+		System.out.println("Hello World!");
+	}
 }
